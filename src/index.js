983c18cbb1f6627eb883eb89fbd9efc6c68f08f6
ostyyy <ggg468432@gmail.com>
2026-04-27 14:46:29 +0300

    Add isPalindrome function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Check Palindrome #1
+
+function isPalindrome(str) {
+    const sep = '';
+    str = str.toLowerCase().replace(/[,'"’.?!\s]/g, sep);
+
+    const strReversed = str.split(sep).reverse().join(sep);
+
+    if (str === strReversed) return true;
+
+    return false;
+}
+
+console.log(isPalindrome('Mountains'));
+console.log(isPalindrome('Did Hannah see bees? Hannah did.'));
+console.log(isPalindrome(' Evil is a name of a foeman, as I live.'));
+console.log(isPalindrome('Madam, in Eden, I’m Adam.'));
