c145b050ce5a7c75ac7ad440703bc982683ba70a
ostyyy <ggg468432@gmail.com>
2026-04-27 15:30:55 +0300

    Add isAnagram function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Check Palindrome #1
-
 function isPalindrome(str) {
     const sep = '';
     str = str.toLowerCase().replace(/[,'"’.?:!\s]/g, sep);
@@ -13,18 +12,18 @@ function isPalindrome(str) {
     return false;
 }
 
-console.log('----Check Palindrome #1----')
+console.log('----Check Palindrome #1----');
+
 console.log(isPalindrome('Mountains'));
 console.log(isPalindrome('Did Hannah see bees? Hannah did.'));
 console.log(isPalindrome(' Evil is a name of a foeman, as I live.'));
 console.log(isPalindrome('Madam, in Eden, I’m Adam.'));
 
 // Check Palindrome #2
-
 function checkPalindrome(str) {
     const sep = '';
 
-    str = str.toLowerCase().replace(/[,'"’.?:!\s]/g, sep);
+    str = str.toLowerCase().replace(/[^a-z]/g, sep);
 
     let left = 0;
     let right = str.length - 1;
@@ -38,7 +37,36 @@ function checkPalindrome(str) {
     }
     return true;
 }
-console.log('----Check Palindrome #2----')
+console.log('----Check Palindrome #2----');
+
 console.log(checkPalindrome('Mr. Owl ate my metal worm.'));
 console.log(checkPalindrome('No sir, away! A papaya war is on.'));
 console.log(checkPalindrome('The Dolomites'));
+
+// Check Anagram #1
+function isAnagram(str1, str2) {
+    const sep = '';
+    str1 = str1.toLowerCase().replace(/[^a-z]/g, sep);
+    str2 = str2.toLowerCase().replace(/[^a-z]/g, sep);
+
+    if (str1.length !== str2.length) return false;
+
+    str1 = str1.split(sep).sort().join(sep);
+    str2 = str2.split(sep).sort().join(sep);
+
+    if (str1 === str2) return true;
+    return false;
+}
+
+console.log('----Check Anagram #1----');
+
+console.log(isAnagram(' Evangelist  ', 'Evil’s Agent'));
+console.log(
+    isAnagram(
+        'The Great New York Rapid Transit Tunnel',
+        'Giant work in street, partly underneath',
+    ),
+);
+
+console.log(isAnagram('dwarf', 'garden'));
+
